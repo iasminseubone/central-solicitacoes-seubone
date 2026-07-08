075eef322c4443ba16dbad818e0395bd23202c82
iasminseubone <iasmin.seubone@gmail.com>
2026-07-08 17:43:08 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,3 @@
+// Único arquivo que você edita depois do deploy do backend.
+// Cole aqui a URL do Apps Script (Passo 1 do README), terminada em /exec.
+const APP_URL = "COLE_AQUI_A_URL_DO_APPS_SCRIPT";
