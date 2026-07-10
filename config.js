ff5ff9549d50477dcf67c64514a188d40627e971
iasminseubone <iasmin.seubone@gmail.com>
2026-07-10 10:15:34 -0300

    Update APP_URL with the correct Apps Script URL

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 // Único arquivo que você edita depois do deploy do backend.
 // Cole aqui a URL do Apps Script (Passo 1 do README), terminada em /exec.
-const APP_URL = "COLE_AQUI_A_URL_DO_APPS_SCRIPT";
+const APP_URL "https://script.google.com/macros/s/AKfycbzi0FUObK71MxGc6FFYPU45Txm3NGWP4etGmig-qh_Vyp4EUUdi2xr_4YU10vkDe9tQ/exec";
+
