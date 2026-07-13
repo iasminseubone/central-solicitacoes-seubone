f28a15e0075f578cfd0a2a5c43bb03f6f036fd26
iasminseubone <iasmin.seubone@gmail.com>
2026-07-13 20:15:59 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,4 +1,3 @@
 // Único arquivo que você edita depois do deploy do backend.
 // Cole aqui a URL do Apps Script (Passo 1 do README), terminada em /exec.
-const APP_URL "https://script.google.com/macros/s/AKfycbzi0FUObK71MxGc6FFYPU45Txm3NGWP4etGmig-qh_Vyp4EUUdi2xr_4YU10vkDe9tQ/exec";
-
+const APP_URL = "https://script.google.com/macros/s/AKfycbzi0FUObK71MxGc6FFYPU45Txm3NGWP4etGmig-qh_Vyp4EUUdi2xr_4YU10vkDe9tQ/exec";
